57e842e31687a780f8aea04f2c40ac16cca7fc82
dominicprzewozny@gmail.com <dominicprzewozny@gmail.com>
2021-03-18 21:35:31 +0000

    Implementing the new item system is becoming quite hard...

---
+++ GameObjects/WeaponAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullRPG.GameObjects
+{
+    class WeaponAttribute
+    {
+        public int MinDamage { get; private set; }
+        public int MaxDamage { get; private set; }
+        public IItem Source { get; set; }
+
+        public WeaponAttribute(IItem source)
+        {
+            Source = source;
+        }
+
+        public void ReceiveMessage<T>(T message)
+        {
+            var weaponMessage = message as WeaponMessage;
+            if (weaponMessage != null)
+            {
+                MinDamage = weaponMessage.MinDamage;
+                MaxDamage = weaponMessage.MaxDamage;
+            }
+        }
+    }
+}
