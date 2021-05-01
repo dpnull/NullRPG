bfc19803266c20accd9cceae7ebddfae156223ba
dominicprzewozny@gmail.com <dominicprzewozny@gmail.com>
2021-05-01 20:00:50 +0100

    Working on actions.

---
+++ GameObjects/Actions/ActionChop.cs
@@ -0,0 +1,38 @@
+﻿using NullRPG.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullRPG.GameObjects.Actions
+{
+    public class ActionChop : BaseAction
+    {
+        private ILocation _location;
+        private IEntity _entity;
+
+        public ActionChop(ILocation location, IEntity entity) : base()
+        {
+
+        }
+
+        public override bool CanInteract()
+        {
+            if (_location.HasComponent<LocationComponents.LocationObjectComponent>())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override void OnInteract()
+        {
+            if (CanInteract())
+            {
+                var obtainable = _location.GetComponent<LocationComponents.LocationObjectComponent>().
+                    LocationObjects///////////TODO its a list
+            }
+        }
+    }
+}
---
+++ GameObjects/Actions/BaseAction.cs
@@ -0,0 +1,30 @@
+﻿using NullRPG.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullRPG.GameObjects.Actions
+{
+    public abstract class BaseAction : IAction
+    {
+        public BaseAction()
+        {
+
+        }
+
+        public virtual void OnInteract()
+        {
+
+        }
+
+        public virtual bool CanInteract
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
